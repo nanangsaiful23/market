a9f7269aecda99af91801f5774f738183abed276
nanangsaiful23 <nanangsaiful23@gmail.com>
2020-06-02 06:15:53 +0700

    build production

---
+++ build/precache-manifest.353f50b66be8b39de1af22648f5dd134.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2879531c9495960ee5159756e7b14aec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95c94cab7818b131abe8",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dd324f578ade7cb17cda",
+    "url": "/static/css/main.2fcfb15f.chunk.css"
+  },
+  {
+    "revision": "95c94cab7818b131abe8",
+    "url": "/static/js/2.a0307d44.chunk.js"
+  },
+  {
+    "revision": "ac708def751a8292e55daf5717e57737",
+    "url": "/static/js/2.a0307d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd324f578ade7cb17cda",
+    "url": "/static/js/main.28a793c3.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "52f4d23ef8065bd6c95aca0f81de67fb",
+    "url": "/static/media/logontn.52f4d23e.png"
+  }
+]);
